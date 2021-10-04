4a9c2c37171e49d718d1e095e18bcda3ec8d94a4
MattRHall <matt.hall85@googlemail.com>
2021-10-04 21:22:23 +0100

    Fourth commit

--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
 # Sudoku Solver
 ------
-### Sudoku Introduction
+## Sudoku Introduction
 A standard sudoku puzzle presents a 9x9 playing board which is partially filled with integers [1,9]. In order to solve the puzzle integers [1,9] must be placed in unfilled spaces so that all rows / columns and 3x3 grids each contain all of the integers [1,9].
 
-### Environment / Observations
-There is no 'best' search algorithm. Instead different approaches are best suited to different situations. Therefore, it is important to fully understand the situation presented to select the right one for the task at hand. With respect to Sudoku I would make the following observations.   
+## Environment / Observations
+There are a number of features about a sudoku puzzle that helps to guide algorithm choice: 
 
 - **We want the goal state not the path** - We only care about the solution to the puzzle, not the route we took to get there. Therefore we do not need an algorithm that record changes in state over time.
 
@@ -16,64 +16,25 @@ There is no 'best' search algorithm. Instead different approaches are best suite
 
 - **The environment is fully observable and deterministic** - We can observe all of the sudoku board at anytime, and we know the impact that adding an integer in a square will have.
 
-### Formal Problem Definition
-Before we consider algorithms, it is useful to properly define the problem. This will help us select the most appropriate approach and provide guidance when implementing an algorithm.
+## Algorithm - Backtracking Search with Forward Checking
+Backtracking search with forward checking is similar to depth-first search. However the search tree is reduced by applying sudoku constraints (also call ARC consistency), to remove states that can't be a solution. Furthermore, the cells with only a small number of possibilities are searched first, to identify and prune states that can't be solutions quickly. For example, if cell-1 can be {1,3} and cell-2 can be {1,5,7,8} then we should search the first cell first. 
 
-- **Variables** - {X<sub>1</sub>...X<sub>n</sub>}, n = integers between [0, 80]. Each variable represents a square in the sudoku board, starting from top left (X<sub>0</sub>) and finishing bottom right (X<sub>80</sub>), moving along a row from left to right, before starting a new row.
+Additional work was done to explore heuristics that could be applied to improve performance by pruning the search tree. This includes searching based on most common integers, and applying naked pairs (f two variables in a row/colum/grid can only be {1,5} then none of the other variables can be {1,5}). However, the increased computational cost of these heuristics did not offset the benefit of a smaller search tree. There are many other different sudoku solving techniques, but checking whether they are applicable at each possible state precludes their use.
 
-- **Domain** - Each variable has the domain {1,2,3,4,5,6,7,8,9} which represent the possible integers that can be placed in a square on a sudoku board.
-
-- **Constraints** - We have 3 global constraints; Alldiff (row), Alldiff (column), Alldiff (grid). Where Alldiff requries all variables to have different values.
-
-### Algorithm Choice
-As outlined above, our algorithm must make use of the sudoku constraints because uninformed / brute search methods would lead to huge search spaces that would be impractical to search. The options we will consider for Sudoku are local search / backtracking search (both of which make use of constraints).
-
-- **Local search** - Local search takes a single state and moves to another state by changing one variable at a time. It requires a heuristic to measure whether the state is improved or not. A logical heuristic would be to count the number of constraint violations (column / row / grid) and select the minimum. The main disadvantage with using local search is the risk of local plateaus and local minimums. This leads the risk of not reaching the goal (which is not acceptable). We would also need to keep track of visited states to ensure we didn't cycle back to a already visited state. 
-
-- **Backtracking search** - Backtracking search will repeatedly choose an unassigned variable, try different values, before extending them in a recursive manner. We can further shrink the search space by eliminating / pruning values (states) that are inconsistent (forward checking). This seems the most suitable approach. We can also explore which heuristics work best, and look at adding advanced sudoku techniques when applying forward checking. 
+## Usage
+The sudoku class, has the method **sudoku_solver** which takes a numpy array (shape=9x9) as an input. The numpy array should have 0's in place of unknown values, and values between 1-9 elsewhere. If there is no solution to the sudoku, or the input is invalid, then a numpy array (shape=9x9) full of -1's
  
-### Algorithm - Backtracking Search with Inference
-The search algorithm we will apply to the sudoku puzzle is **backtracking search** with inference (**forward checking**). Forward checking will apply arc consistency to all variables, to ensure no variables are inconsistent with each other. We will also apply **minimum remaining values** when selecting variables. Pseudocode for the approach is laid out below. 
-
-* Constraint-Propagation
-    -    Run constraint propagation (ARC consistency), return state.  
-    - If state is a final solution, **return** answer if valid, else return array of -1's.  
-
-* Run backtracking search
-    - Select variable with lowest number of potential values in its domain.  
-    - Select the lowest value in the domain of this variable.  
-    - Set the variable to this value.  
-    - Apply constraint propagation (ARC consistency) to remaining variables.  
-    - If state is a final solution, **return** answer if valid, else return array of -1's.  
-    - If state is consistent (but not complete) then repeat backtracking search.  
-
-### Algorithm - Failed Enchancements
-**Selecting value by most common occurence** - By focusing on the most common integers on the sudoku board that have been finalized it might be possible to find solutions more quickly. Although it did work in some instances, the computational cost of building and sorting a dictionary in every iteration was too great.
 ```
-def get_unique_value_count(self):
-        unique, counts = np.unique(self.final, return_counts = True)
-        return dict(zip(unique, counts))
+puzzle = np.array([[0, 8, 5, 0, 1, 3, 0, 0, 9],
+       [6, 3, 4, 0, 0, 2, 1, 7, 5],
+       [0, 2, 0, 5, 7, 4, 0, 3, 0],
+       [2, 4, 8, 3, 6, 7, 9, 5, 1],
+       [9, 6, 0, 4, 5, 8, 0, 2, 3],
+       [3, 5, 7, 2, 0, 0, 4, 8, 0],
+       [5, 7, 3, 1, 0, 0, 8, 9, 2],
+       [4, 9, 6, 0, 2, 5, 3, 1, 0],
+       [8, 1, 2, 0, 3, 9, 5, 6, 4]])
 
-common_values = partial_state.get_unique_value_count()
-ordered_values = []
-for temp_value in values:
-    ordered_values.append((temp_value, common_values.get(temp_value,0)))
-    ordered_values.sort(key = lambda x: x[1], reverse = True)
+game = sudoku()
+game.sudoku_solver(puzzle)
 ```
-**Naked pairs** - The objective here was to improve on the constraint propagation by looking for naked pairs, i.e. if two variables in a row/colum/grid can only be {1,5} then none of the other variables can be {1,5}. Disappointingly this failed to make a meaningful improvement. Although it did improve in some instances, the computational cost was too great. As a result of this I didn't build naked triples or other sudoku solving techniques (naked pairs should be the most beneficial).
-```
-    def naked_pair(self, positions): #positions = array of 9 positions
-        naked_pairs = []
-        for pos1 in positions:
-            for pos2 in positions:
-                if pos1 != pos2:
-                    if self.final[pos1] == -1 and self.final[pos2] == -1:
-                        if len(self.get_possible_values(pos1)) == 2 and len(self.get_possible_values(pos1)) == 2:
-                            if all(self.possibilities[pos1] == self.possibilities[pos2]):
-                                if (pos1, pos2) not in naked_pairs and (pos2, pos1) not in naked_pairs:
-                                    naked_pairs.append((pos1, pos2))
-```
-
-### Further work
-Further work would focus on improved implementation of the algorithm (identify bottlenecks, and small changes to data structure choices) that could lead to improvements in speed. We would also try using lists as data structures instead of numpy arrays (given no matrix multiplication / operations are actually performed). Finally, we would explore additional heuristics as the choice of the heuristic could make a big difference to computational time (e.g. maximum cardinality ordering, minimum bandwidth ordering).
-
--- sudoku.py
+++ sudoku.py
@@ -1,125 +1,125 @@
 import numpy as np
 import copy
 
-class board():
-    
-    def __init__(self, board):
-        """ Creates board class with attributes final / possibilities"""
-        self.start = board.flatten()
-        self.final = np.array([-1 if i == 0 else i for i in np.nditer(self.start)])
-        
-        self.possibilities = np.array([range(1,10) if _ == 0 else [0] * 9 for _ in np.nditer(self.start)])
-        for i,v in np.ndenumerate(self.start):
-            if v != 0:
-                self.possibilities[i[0]][v - 1] = v
-            else:
-                cells = self.get_col_row_grid(i[0])
-                for idx in set(cells[0] + cells[1] + cells[2]):
-                    if self.final[idx] != -1:
-                        self.possibilities[i[0]][self.final[idx]-1] = 0
-                        
-    @staticmethod
-    def get_col_row_grid(position):
-        """ Returns the column / row / grid indices for a given position """
-        col_idx = [(9*_)+ position%9 for _ in range(9)]
-        row_idx = list(range(position-(position%9),position-(position%9)+9))
-        col_pos = position%9 - position%9%3
-        row_pos = ((position//9)*9) - ((position//9)*9)%27
-        grid_idx = [col_pos + row_pos + _ for _ in range(3)] + \
-                    [col_pos + row_pos + _ for _ in range(9,12)] + \
-                    [col_pos + row_pos + _ for _ in range(18,21)]
-        return col_idx, row_idx, grid_idx
-    
-    def get_unique_value_count(self):
-        unique, counts = np.unique(self.final, return_counts = True)
-        return dict(zip(unique, counts))
-        
-    def is_valid_answer(self):
-        """ Returns True if sudoku with 1-9 in rows, 1-9 in columns and 1-9 in boxes """
-        rows = [set([self.final[i+(9*j)] for i in range(0,9)]) == set(range(1,10)) for j in range(0,9)]
-        cols = [set([self.final[(9*i)+j] for i in range(0,9)]) == set(range(1,10)) for j in range(0,9)]
-        grid = [set([self.final[col + row + _] for _ in range(3)] +
-                 [self.final[col + row + _] for _ in range(9,12)] +
-                 [self.final[col + row + _] for _ in range(18,21)]) == set(range(1,10)) for col in [0,3,6] for row in [0, 27, 54]]
-
-        return self.final.reshape(9,9) if all(rows) == True and all(cols) == True and all(grid) == True else np.full((9,9),-1)
-     
-    def is_goal(self):
-        """ Returns True if all values in the final array are not -1 """
-        return True if np.all(self.final != -1) else False
-      
-    def is_invalid(self):
-        """ Returns False if there is an array in possibilities full of 0's """
-        return True if np.any([not np.any(i) for i in self.possibilities]) else False
-    
-    def get_singletons(self):
-        """ Returns the positions where we have a 'certain' value but final not updated """
-        return [index[0] for index,n in np.ndenumerate(np.count_nonzero(self.possibilities == 0, axis = 1)) if (self.final[index] == -1 and n == 8)]
-        
-    def get_possible_values(self, position):
-        """ Returns the possible value from a given position (excludes 0) """
-        arr = self.possibilities[position]
-        return arr[np.where(arr != 0)].copy()
-    
-    def set_value(self, position, value): #position(0-80)
-        """ Creates new state, adds in position, checks if further improvements can be made """
-
-        state = copy.deepcopy(self)
-        state.possibilities[position] = [0 if i != value else value for i in range(1,10)]
-        state.final[position] = value
-
-        # REMOVE FROM ROW / COLUMNS / GRID POSSIBILITIES
-        col_row_grid = self.get_col_row_grid(position)
-        for pos in set(col_row_grid[0] + col_row_grid[1] + col_row_grid[2]):
-            if pos != position:
-                if self.final[pos] == -1:
-                    state.possibilities[pos][value - 1] = 0
-                           
-        return self.set_value_loop(state)
-    
-    def set_value_loop(self, state):
-        """ Recrusive loop to turn all singletons into final values (and then reset)"""
-        singleton_positions = state.get_singletons()
-        while len(singleton_positions) > 0:
-            new_pos = singleton_positions[0]
-            state = state.set_value(new_pos, state.get_possible_values(new_pos)[0])
-            singleton_positions = state.get_singletons()
-        
-        return state
-
 class sudoku():
     def __init__(self):
         pass
 
-    def pick_next_position(self, partial_state):
+    class board():
+
+        def __init__(self, board):
+            """ Creates board class with attributes final / possibilities"""
+            self.start = board.flatten()
+            self.final = np.array([-1 if i == 0 else i for i in np.nditer(self.start)])
+
+            self.possibilities = np.array([range(1,10) if _ == 0 else [0] * 9 for _ in np.nditer(self.start)])
+            for i,v in np.ndenumerate(self.start):
+                if v != 0:
+                    self.possibilities[i[0]][v - 1] = v
+                else:
+                    cells = self._get_col_row_grid(i[0])
+                    for idx in set(cells[0] + cells[1] + cells[2]):
+                        if self.final[idx] != -1:
+                            self.possibilities[i[0]][self.final[idx]-1] = 0
+
+        @staticmethod
+        def _get_col_row_grid(position):
+            """ Returns the column / row / grid indices for a given position """
+            col_idx = [(9*_)+ position%9 for _ in range(9)]
+            row_idx = list(range(position-(position%9),position-(position%9)+9))
+            col_pos = position%9 - position%9%3
+            row_pos = ((position//9)*9) - ((position//9)*9)%27
+            grid_idx = [col_pos + row_pos + _ for _ in range(3)] + \
+                        [col_pos + row_pos + _ for _ in range(9,12)] + \
+                        [col_pos + row_pos + _ for _ in range(18,21)]
+            return col_idx, row_idx, grid_idx
+
+        def _get_unique_value_count(self):
+            unique, counts = np.unique(self.final, return_counts = True)
+            return dict(zip(unique, counts))
+
+        def _is_valid_answer(self):
+            """ Returns True if sudoku with 1-9 in rows, 1-9 in columns and 1-9 in boxes """
+            rows = [set([self.final[i+(9*j)] for i in range(0,9)]) == set(range(1,10)) for j in range(0,9)]
+            cols = [set([self.final[(9*i)+j] for i in range(0,9)]) == set(range(1,10)) for j in range(0,9)]
+            grid = [set([self.final[col + row + _] for _ in range(3)] +
+                     [self.final[col + row + _] for _ in range(9,12)] +
+                     [self.final[col + row + _] for _ in range(18,21)]) == set(range(1,10)) for col in [0,3,6] for row in [0, 27, 54]]
+
+            return self.final.reshape(9,9) if all(rows) == True and all(cols) == True and all(grid) == True else np.full((9,9),-1)
+
+        def _is_goal(self):
+            """ Returns True if all values in the final array are not -1 """
+            return True if np.all(self.final != -1) else False
+
+        def _is_invalid(self):
+            """ Returns False if there is an array in possibilities full of 0's """
+            return True if np.any([not np.any(i) for i in self.possibilities]) else False
+
+        def _get_singletons(self):
+            """ Returns the positions where we have a 'certain' value but final not updated """
+            return [index[0] for index,n in np.ndenumerate(np.count_nonzero(self.possibilities == 0, axis = 1)) if (self.final[index] == -1 and n == 8)]
+
+        def _get_possible_values(self, position):
+            """ Returns the possible value from a given position (excludes 0) """
+            arr = self.possibilities[position]
+            return arr[np.where(arr != 0)].copy()
+
+        def _set_value(self, position, value): #position(0-80)
+            """ Creates new state, adds in position, checks if further improvements can be made """
+
+            state = copy.deepcopy(self)
+            state.possibilities[position] = [0 if i != value else value for i in range(1,10)]
+            state.final[position] = value
+
+            # REMOVE FROM ROW / COLUMNS / GRID POSSIBILITIES
+            col_row_grid = self._get_col_row_grid(position)
+            for pos in set(col_row_grid[0] + col_row_grid[1] + col_row_grid[2]):
+                if pos != position:
+                    if self.final[pos] == -1:
+                        state.possibilities[pos][value - 1] = 0
+
+            return self._set_value_loop(state)
+
+        def _set_value_loop(self, state):
+            """ Recrusive loop to turn all singletons into final values (and then reset)"""
+            singleton_positions = state._get_singletons()
+            while len(singleton_positions) > 0:
+                new_pos = singleton_positions[0]
+                state = state._set_value(new_pos, state._get_possible_values(new_pos)[0])
+                singleton_positions = state._get_singletons()
+
+            return state
+
+    def _pick_next_position(self, partial_state):
         """ Extracts the positions where more than 1 non-zero value, selects a position randomly """
         pos_idx = [i for i in range(len(partial_state.possibilities)) if np.count_nonzero(partial_state.possibilities[i]) > 1]
         return pos_idx
 
-    def order_values(self, partial_state, position):
+    def _order_values(self, partial_state, position):
         """ Returns the possible values to try for a given position and state """
-        values = partial_state.get_possible_values(position)
+        values = partial_state._get_possible_values(position)
         return values
     
-    def depth_first_search(self, partial_state):
+    def _depth_first_search(self, partial_state):
         """ Perform depth first search, starting with least possibles / lowest values (to force answer)"""
 
-        position = self.pick_next_position(partial_state)
+        position = self._pick_next_position(partial_state)
 
         if position == []:
             return partial_state
         else:
             position = np.sort(position)[0]
 
-        values = self.order_values(partial_state, position)
+        values = self._order_values(partial_state, position)
 
         for value in np.sort(values)[::-1]:
-            new_state = partial_state.set_value(position, value)
-            if new_state.is_goal():
+            new_state = partial_state._set_value(position, value)
+            if new_state._is_goal():
                 return new_state
-            if not new_state.is_invalid():
-                deep_state = self.depth_first_search(new_state)
-                if deep_state is not None and deep_state.is_goal():
+            if not new_state._is_invalid():
+                deep_state = self._depth_first_search(new_state)
+                if deep_state is not None and deep_state._is_goal():
                     return deep_state
         return None
 
@@ -136,18 +136,20 @@ class sudoku():
                 It contains the solution, if there is one. If there is no solution, all array entries should be -1.
         """
 
-        partial_state = board(sudoku)
+        partial_state = self.board(sudoku)
 
-        singleton_positions = partial_state.get_singletons()
+        singleton_positions = partial_state._get_singletons()
         while len(singleton_positions) > 0:
             new_pos = singleton_positions[0]
-            partial_state = partial_state.set_value(new_pos, partial_state.get_possible_values(new_pos)[0])
-            singleton_positions = partial_state.get_singletons()
+            partial_state = partial_state._set_value(new_pos, partial_state._get_possible_values(new_pos)[0])
+            singleton_positions = partial_state._get_singletons()
 
-        if partial_state.is_goal():
-            return partial_state.is_valid_answer()
+        if partial_state._is_goal():
+            return partial_state._is_valid_answer()
 
-        answer = self.depth_first_search(partial_state)
-        return np.full((9,9),-1) if answer == None else answer.is_valid_answer()
+        answer = self._depth_first_search(partial_state)
+        return np.full((9,9),-1) if answer == None else answer._is_valid_answer()
+
+
 
 
